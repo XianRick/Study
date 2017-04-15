da3ee33361682d87224fce5ebd9ba11bc4f076b7
XianRick <1146894003@qq.com>
2017-04-15 10:58:34 +0800

    Property Study

---
+++ app/src/main/java/com/exe/study/propertyactivity/ObjectActivity.java
@@ -0,0 +1,57 @@
+package com.exe.study.propertyactivity;
+
+import android.animation.ObjectAnimator;
+import android.animation.PropertyValuesHolder;
+import android.animation.ValueAnimator;
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import com.exe.study.R;
+
+/**
+ * @Function
+ * @Author Xian
+ * @Data 2017/4/15 10:11
+ */
+
+public class ObjectActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_object);
+        final ImageView iv = (ImageView) findViewById(R.id.iv);
+        Button btn = (Button) findViewById(R.id.btn);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //一 自定义实现属性动画
+                //实现透明图渐变动画
+                //ObjectAnimator.ofFloat(iv, "alpha", 1.0f, 0.3f).setDuration(2000).start();
+                //实现旋转动画
+                //ObjectAnimator.ofFloat(iv, "rotationX", 0.0f, 30f).setDuration(2000).start();
+                //二 自定义+监听实现属性动画
+                /*ObjectAnimator animator = ObjectAnimator.ofFloat(iv, "xian", 1.0f, 0.3f);
+                animator.setDuration(3000);
+                animator.start();
+                animator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+                    @Override
+                    public void onAnimationUpdate(ValueAnimator animation) {
+                        float anim = (float) animation.getAnimatedValue();
+                        iv.setAlpha(anim);
+                        iv.setScaleX(anim);
+                        iv.setScaleY(anim);
+                    }
+                });*/
+                //三 使用PropertyViewHolder实现属性动画
+                PropertyValuesHolder alpha = PropertyValuesHolder.ofFloat("alpha", 1f, 0.3f);
+                PropertyValuesHolder rotationX = PropertyValuesHolder.ofFloat("rotationX", 0f, 30f);
+                PropertyValuesHolder rotationY = PropertyValuesHolder.ofFloat("rotationY", 0f, 30f);
+                ObjectAnimator.ofPropertyValuesHolder(iv, alpha, rotationX, rotationY);
+            }
+        });
+
+    }
+}
---
+++ app/src/main/java/com/exe/study/propertyactivity/ValueActivity.java
@@ -0,0 +1,89 @@
+package com.exe.study.propertyactivity;
+
+import android.animation.TypeEvaluator;
+import android.animation.ValueAnimator;
+import android.app.Activity;
+import android.graphics.PointF;
+import android.os.Bundle;
+import android.util.DisplayMetrics;
+import android.view.View;
+import android.view.animation.LinearInterpolator;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import com.exe.study.R;
+
+/**
+ * @Function
+ * @Author Xian
+ * @Data 2017/4/15 10:35
+ */
+
+public class ValueActivity extends Activity implements View.OnClickListener {
+
+    private ImageView iv;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_value);
+        iv = (ImageView) findViewById(R.id.iv);
+        Button btn_verticalRun = (Button) findViewById(R.id.btn_verticalRun);
+        Button btn_parabola = (Button) findViewById(R.id.btn_parabola);
+        btn_verticalRun.setOnClickListener(this);
+        btn_parabola.setOnClickListener(this);
+
+
+    }
+
+    public int getWindowsHeight() {
+        //获取屏幕
+        DisplayMetrics metrics = new DisplayMetrics();
+        getWindowManager().getDefaultDisplay().getMetrics(metrics);
+        int heightPixels = metrics.heightPixels;
+        return heightPixels;
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.btn_verticalRun:
+                ValueAnimator animator = ValueAnimator.ofFloat(0, getWindowsHeight() - iv.getHeight());
+                animator.setTarget(iv);
+                animator.setDuration(3000).start();
+                animator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+                    @Override
+                    public void onAnimationUpdate(ValueAnimator animation) {
+                        float height = (float) animation.getAnimatedValue();
+                        iv.setRotationY(height);
+                    }
+                });
+                break;
+            case R.id.btn_parabola:
+                ValueAnimator animator1 = new ValueAnimator();
+                animator1.setDuration(3000);
+                animator1.setObjectValues(new PointF(0, 0));
+                animator1.setInterpolator(new LinearInterpolator());
+                animator1.setEvaluator(new TypeEvaluator<PointF>() {
+
+                    @Override
+                    public PointF evaluate(float fraction, PointF startValue, PointF endValue) {
+                        PointF pointF = new PointF();
+                        pointF.x = 200 * fraction * 3;
+                        pointF.y = 100 * (fraction * 3) * (fraction * 3);
+                        return pointF;
+                    }
+                });
+                animator1.start();
+                animator1.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+                    @Override
+                    public void onAnimationUpdate(ValueAnimator animation) {
+                        PointF pointF = (PointF) animation.getAnimatedValue();
+                        iv.setX(pointF.x);
+                        iv.setY(pointF.y);
+                    }
+                });
+                break;
+        }
+    }
+}
